40ae16cdbc3021d65bcc2c3abf3d834573e9afd4
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2019-07-18 16:32:06 -0500

    feature 2 created Muestra el input del usuario

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var os = navigator.platform; 
 document.getElementById("msg").innerText='Hello, your os is '+ os;
+document.getElementById("myForm").addEventListener('submit', (e)=>{e.preventDefault()});
 
 function changeValueById(id, newValue){
     document.getElementById(id).innerHTML=newValue;
+}
+
+function onFormSubmit(){
+    changeValueById("txt", "Hola " + document.getElementById("txtName").value);
 }
